5d05a7e8ae6542fa1a104e561905ba005f6a79c9
Anthony Cabacungan <antcabacungan@csu.fullerton.edu>
2023-11-29 14:17:40 -0800

    code refactoring

--- cli.py
+++ cli.py
@@ -16,33 +16,35 @@ import sys
 def ftp_quit(client_socket):
     # disconnects from the server and exits
     clientSocket.close()
+    print("Disconnected from server")
     sys.exit()
 
 if __name__ == "__main__":
     # TODO: input validation / error handling wherever needed
 
+    if len(sys.argv) != 3:
+        print("Usage: python cli.py <server machine> <server port>")
+        sys.exit()
+    
     serverMachine = sys.argv[1] 
     serverPort = sys.argv[2]
 
-    # DNS lookup to get IP address
-    ip = socket.gethostbyname(serverMachine)
-
-    # create socket 
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     try:
-        # TODO: connect to server
+        # DNS lookup to get IP address
+        ip = socket.gethostbyname(serverMachine)
+
+        # create socket 
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # connect to server
         clientSocket.connect((ip, serverPort))
-    except:
-        print("Cannot connect to server")        
-    
-    # TODO: Upon connecting to the server, the client prints out ftp>, which allows the user to execute
-    # the following commands.
-        # ftp> get <file name> (downloads file <file name> from the server)
-        # ftp> put <filename> (uploads file <file name> to the server)
-        # ftp> ls(lists files on theserver)
-        # ftp> quit (disconnects from the server and exits)
-    if (clientSocket):
+
+        # Upon connecting to the server, the client prints out ftp>, which allows the user to execute
+        # the following commands.
+            # ftp> get <file name> (downloads file <file name> from the server)
+            # ftp> put <filename> (uploads file <file name> to the server)
+            # ftp> ls(lists files on theserver)
+            # ftp> quit (disconnects from the server and exits)
         print("Execute one of the following commands: ")
         print("ftp get <file name>  : downloads file <file name> from the server")
         print("ftp put <file name>  : uploads file <file name> to the server")
@@ -61,7 +63,7 @@ if __name__ == "__main__":
                 elif arguments[1] == "ls":
                     print("put")
                 elif arguments[1] == "quit":
-                    print("quit")
+                    ftp_quit(clientSocket)
                     break
-
-        ftp_quit(clientSocket)
+    except:
+        print("Cannot connect to server")
