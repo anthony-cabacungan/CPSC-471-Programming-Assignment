235e62efabe2e67a1304c80c6614a1bebf85062f
Anthony Cabacungan <antcabacungan@csu.fullerton.edu>
2023-11-30 13:11:19 -0800

    set up basic server

--- serv.py
+++ serv.py
@@ -5,7 +5,25 @@ import sys
 
 # TODO: input validation / error handling wherever needed
 
-serverPort = sys.argv[1]
+if len(sys.argv) != 2:
+    print("Usage: python serv.py <PORTNUMBER>")
+    sys.exit()
+
+serverPort = int(sys.argv[1])
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serverSocket.bind(('', serverPort))
+
+serverSocket.listen(1)
+
+while 1:
+    try: 
+        print("Waiting for a connection...")
+        clientSock, addr = serverSocket.accept()
+        print("Accepted connection")
+    except:
+        print("Could not connect")
 
 if serverPort:
     # TODO: setup server
